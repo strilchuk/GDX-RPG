6c85f8c4c4b278d3ad3b791fc885ca08de4440e1
Alexander <strilchukalexander@gmail.com>
2018-09-11 17:37:49 +0300

    1 fork

---
+++ core/src/com/rpsg/rpg/system/ui/TextButton.java
@@ -0,0 +1,30 @@
+package com.rpsg.rpg.system.ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.rpsg.gdxQuery.$;
+import com.rpsg.gdxQuery.GdxQuery;
+
+/** A button with a child {@link Label} to display text. * @author Nathan Sweet */
+public class TextButton extends com.badlogic.gdx.scenes.scene2d.ui.TextButton {
+
+    private GdxQuery query;
+
+    public TextButton(String text, TextButtonStyle style) {
+        super(text, style);
+        query = $.add(this);
+    }
+
+    public TextButton check(boolean b){
+        this.setChecked(b);
+        return this;
+    }
+
+    public TextButton onClick(Runnable run){
+        return query.click(run).getItem(getClass());
+    }
+
+    public TextButton click(){
+        return query.click().getItem(getClass());
+    }
+
+}
